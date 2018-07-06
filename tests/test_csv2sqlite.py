876e1606a2562424410d8d31af6be545f1294ffd
Mike Silva <mike.a.silva@gmail.com>
2018-07-06 14:46:59 -0400

    Fixed Flake8 Errors

--- tests/test_csv2sqlite.py
+++ tests/test_csv2sqlite.py
@@ -11,12 +11,14 @@ from csv2sqlite.cli import convert
 DEFAULT_TABLE_NAME = 'data'
 TEMP_DB_PATH = os.path.join(tempfile.gettempdir(), 'csv2sqlite-test.sqlite')
 
+
 class Csv2SqliteTestCase(unittest.TestCase):
     def setUp(self):
         if os.path.exists(TEMP_DB_PATH):
             os.remove(TEMP_DB_PATH)
 
-    def convert_csv(self, csv, table = DEFAULT_TABLE_NAME, headers = None, types = None):
+    def convert_csv(self, csv, table=DEFAULT_TABLE_NAME, headers=None,
+                    types=None):
         '''Converts a CSV to a SQLite database and returns a database cursor
            into the resulting file'''
         dbpath = TEMP_DB_PATH
@@ -35,15 +37,15 @@ class Csv2SqliteTestCase(unittest.TestCase):
         else:
             typeObj = None
         try:
-            infile = StringIO(csv) # Python 3
-        except:
+            infile = StringIO(csv)  # Python 3
+        except TypeError:
             infile = BytesIO(csv)  # Python 2
         convert(infile, dbpath, table, headerObj, None, typeObj)
         conn = sqlite3.connect(dbpath)
         c = conn.cursor()
         return c
 
-    def convert_file(self, path, table = DEFAULT_TABLE_NAME, compression = None):
+    def convert_file(self, path, table=DEFAULT_TABLE_NAME, compression=None):
         '''Converts a CSV file to a SQLite database and returns a database cursor
            into the resulting file'''
         dbpath = TEMP_DB_PATH
@@ -54,25 +56,23 @@ class Csv2SqliteTestCase(unittest.TestCase):
 
     def test(self):
         '''Simple test case'''
-        c = self.convert_csv(
-    '''heading_1,heading_2,heading_3
-    abc,1,1.0
-    xyz,2,2.0
-    efg,3,3.0'''
-        )
-        c.execute('SELECT COUNT(heading_3) FROM %s' % DEFAULT_TABLE_NAME);
+        csv = '''heading_1,heading_2,heading_3
+        abc,1,1.0
+        xyz,2,2.0
+        efg,3,3.0'''
+        c = self.convert_csv(csv)
+        c.execute('SELECT COUNT(heading_3) FROM %s' % DEFAULT_TABLE_NAME)
         row = next(c)
         self.assertEqual(row[0], 3)
 
     def test_semicolon(self):
         '''Simple test case (semicolon)'''
-        c = self.convert_csv(
-    '''heading_1;heading_2;heading_3
-    abc;1;1,0
-    xyz;2;2,0
-    efg;3;3,0'''
-        )
-        c.execute('SELECT COUNT(heading_3) FROM %s' % DEFAULT_TABLE_NAME);
+        csv = '''heading_1;heading_2;heading_3
+        abc;1;1,0
+        xyz;2;2,0
+        efg;3;3,0'''
+        c = self.convert_csv(csv)
+        c.execute('SELECT COUNT(heading_3) FROM %s' % DEFAULT_TABLE_NAME)
         row = next(c)
         self.assertEqual(row[0], 3)
 
@@ -82,15 +82,17 @@ class Csv2SqliteTestCase(unittest.TestCase):
               xyz,2,2,1'''
         headers = '''heading_1,heading_2,heading_3,heading_4'''
         c = self.convert_csv(csv, headers=headers)
-        c.execute('SELECT COUNT(heading_3) FROM %s' % DEFAULT_TABLE_NAME);
+        c.execute('SELECT COUNT(heading_3) FROM %s' % DEFAULT_TABLE_NAME)
         row = next(c)
         self.assertEqual(row[0], 2)
 
     def test_strips_headers(self):
         c = self.convert_csv('''col_a    , col_b''')
-        c.execute('SELECT sql FROM sqlite_master WHERE name = "%s"' % DEFAULT_TABLE_NAME)
+        c.execute('SELECT sql FROM sqlite_master WHERE name = "%s"' %
+                  DEFAULT_TABLE_NAME)
         row = next(c)
-        self.assertEqual(row[0], 'CREATE TABLE data ("col_a" text,"col_b" text)')
+        self.assertEqual(row[0],
+                         'CREATE TABLE data ("col_a" text,"col_b" text)')
 
     def test_separate_types(self):
         '''Types passed separately test case.'''
@@ -107,15 +109,16 @@ class Csv2SqliteTestCase(unittest.TestCase):
 
     def test_ignores_nulls_when_guessing_col_types(self):
         '''Ignore nulls when guessing column types'''
-        c = self.convert_csv(
-    '''col_a, col_b
-    ,
-    ,
-    1,text'''
-        )
-        c.execute('SELECT sql FROM sqlite_master WHERE name = "%s"' % DEFAULT_TABLE_NAME)
+        csv = '''col_a, col_b
+            ,
+            ,
+            1,text'''
+        c = self.convert_csv(csv)
+        c.execute('SELECT sql FROM sqlite_master WHERE name = "%s"' %
+                  DEFAULT_TABLE_NAME)
         row = next(c)
-        self.assertEqual(row[0], 'CREATE TABLE data ("col_a" integer,"col_b" text)')
+        self.assertEqual(row[0],
+                         'CREATE TABLE data ("col_a" integer,"col_b" text)')
 
     def test_csv_file(self):
         '''Should handle uncompressed files.'''
@@ -170,6 +173,7 @@ class Csv2SqliteTestCase(unittest.TestCase):
         row = next(c)
         self.assertEqual(row[0], 3)
         os.remove(bz2File)
+
 
 if __name__ == '__main__':
     unittest.main()
